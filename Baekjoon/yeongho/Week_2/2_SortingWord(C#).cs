0fcae13ba4a841cb359801d5ae4334f15e4f7b89
95_CYH <150199521+GrowingPains228@users.noreply.github.com>
2025-01-07 14:38:08 +0900

    [Baekjoon-Level2-Week2] 단어 정렬
    C# 스크립트

---
+++ Baekjoon/yeongho/Week_2/2_SortingWord(C#).cs
@@ -0,0 +1,58 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        SortingWord tool = new SortingWord();
+
+        //초기화 및 입력
+        tool.Initialize();
+        
+        // 시뮬레이션 시작
+        tool.StartingSort();
+        
+        // 프린트
+        tool.PrintResult();
+    }
+}
+
+public class SortingWord
+{
+    private List<string> words;
+
+    public void Initialize()
+    {
+        var length = int.Parse(Console.ReadLine());
+        var hashsetWords = new HashSet<string>();
+        words = new List<string>();
+        for (int i = 0; i < length; i++)
+        {
+            var word = Console.ReadLine();
+            if (hashsetWords.Contains(word)) continue;
+            hashsetWords.Add(word);
+            words.Add(word);
+        }
+    }
+
+    public void StartingSort()
+    {
+        words.Sort((word1, word2) =>
+        {
+            if (word1.Length < word2.Length)
+                return -1;
+            else if (word1.Length == word2.Length)
+            {
+                return string.Compare(word1, word2);
+            }
+
+            return 1;
+        });
+    }
+
+    public void PrintResult()
+    {
+        foreach (var word in words)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
