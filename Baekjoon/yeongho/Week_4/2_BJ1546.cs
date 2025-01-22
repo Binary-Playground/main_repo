ab79f37e032a534010d6f103166ebae00cf53c59
95_CYH <150199521+GrowingPains228@users.noreply.github.com>
2025-01-22 16:35:29 +0900

    [Baekjoon-Level2-Week4]평균
    C#

---
+++ Baekjoon/yeongho/Week_4/2_BJ1546.cs
@@ -0,0 +1,25 @@
+﻿public class BJ1546 : IBaekjoon
+{
+    private float[] scores;
+    
+    public void Initialize()
+    {
+        int N = int.Parse(Console.ReadLine());
+        scores = new float[N];
+        scores = Array.ConvertAll(Console.ReadLine().Split(), float.Parse);
+    }
+
+    public void Play()
+    {
+        float maxNum = scores.Max();
+        for (int i = 0; i < scores.Length; i++)
+        {
+            scores[i] = scores[i] / maxNum * 100;
+        }
+    }
+
+    public void Print()
+    {
+        Console.WriteLine(scores.Average());
+    }
+}
