174b56bed917729978491e65a669ffe21a10450c
95_CYH <150199521+GrowingPains228@users.noreply.github.com>
2025-01-23 09:19:25 +0900

    [Baekjoon-Level2-Week4]1로 만들기
    C#

---
+++ Baekjoon/yeongho/Week_4/3_BJ1463.cs
@@ -0,0 +1,42 @@
+﻿public class BJ1463 : IBaekjoon
+{
+    private int number;
+    private int ans;
+    
+
+    public void Initialize()
+    {
+        number = int.Parse(Console.ReadLine().Trim());
+    }
+
+    public void Play()
+    {
+        ans = GetCalculate(number);
+    }
+
+    public void Print()
+    {
+        Console.WriteLine(ans);
+    }
+
+    private int GetCalculate(int num)
+    {
+        int[] dp = new int[num + 1];
+        for (int i = 0; i < num+1; i++)
+            dp[i] = int.MaxValue;
+
+        dp[^1] = 0;
+        for (int i = num; i >= 1; i--)
+        {
+            if (i % 3 == 0)
+                dp[i / 3] = Math.Min(dp[i / 3], dp[i] + 1);
+            
+            if(i%2 == 0)
+                dp[i / 2] = Math.Min(dp[i / 2], dp[i] + 1);
+            
+            dp[i - 1] = Math.Min(dp[i - 1], dp[i] + 1);
+        }
+
+        return dp[1];
+    }
+}
