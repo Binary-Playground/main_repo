c62bc47272f2615175571b0b29c4720f87095561
95_CYH <150199521+GrowingPains228@users.noreply.github.com>
2025-02-05 15:45:33 +0900

    [Baekjoon-Level2-Week5,6] 수 찾기, 소수 찾기

---
+++ Baekjoon/yeongho/Week_5/2_BJ1920.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+public class BJ1920 : IBaekjoon
+{
+    private int n, m;
+    private int[] targetArray;
+    private HashSet<int> _hashSet;
+    public void Initialize()
+    {
+        n = int.Parse(Console.ReadLine().Trim());
+        
+        _hashSet = new HashSet<int>();
+        string[] elements = Console.ReadLine().Trim().Split();
+        foreach (string element in elements)
+            _hashSet.Add(int.Parse(element));
+        
+        m = int.Parse(Console.ReadLine().Trim());
+        targetArray = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+    }
+
+    public void Play()
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++)
+        {
+            sb.AppendLine(_hashSet.Contains(targetArray[i]) ? "1" : "0");
+        }
+        Console.WriteLine(sb.ToString());
+    }
+
+    public void Print()
+    {
+        
+    }
+}
---
+++ Baekjoon/yeongho/Week_6/2_BJ1978.cs
@@ -0,0 +1,47 @@
+﻿public class BJ1978 : IBaekjoon
+{
+    private bool[] notPrimeNumvers = new bool[1001];
+    private int n, ans;
+    private int[] numbers;
+    private HashSet<int> primeNumSet;
+    public void Initialize()
+    {
+        n = int.Parse(Console.ReadLine().Trim());
+        numbers = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+        ans = 0;
+        primeNumSet = new HashSet<int>();
+        notPrimeNumvers[0] = true;
+        notPrimeNumvers[1] = true;
+        for (int i = 2; i <= 1000; i++)
+        {
+            if (notPrimeNumvers[i]) continue;
+
+            primeNumSet.Add(i);
+            SetPrimeNum(i, 2);
+        }
+    }
+
+    public void Play()
+    {
+        foreach (int number in numbers)
+        {
+            if (!primeNumSet.Contains(number)) continue;
+
+            ans += 1;
+        }
+    }
+    
+    public void Print()
+    {
+        Console.WriteLine(ans);
+    }
+    
+    private void SetPrimeNum(int num, int cnt)
+    {
+        if (num * cnt >1000)
+            return;
+
+        notPrimeNumvers[num * cnt] = true;
+        SetPrimeNum(num, cnt+1);
+    }
+}
